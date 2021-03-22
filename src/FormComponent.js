2e62adcdd71bc6d7da777deecc468f7caa14e00b
Martin W <willkomm@posteo.de>
2021-03-22 20:45:13 +0100

    works now. without submit.

--- src/FormComponent.js
+++ src/FormComponent.js
@@ -3,23 +3,27 @@ import { Form, FormControl } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
 
 
-
 export default class FormComponent extends Component{
     constructor() {
         super();
         this.state = {
             vatId: null,
-            hasError: false,
             errors: {
-                vatId: '',
+                vatIdValid: false,
+                vatIdInValid: false
             }
         };  
         this.validateInput = this.validateInput.bind(this)
-        //const vatId = 1234;
+        this.handleChange = this.handleChange.bind(this)
     }
-        
+    
+    handleChange = event => {
+        this.setState({
+            vatId: event.target.value
+        });
+    }
+
     validateInput() {
-        console.log('state: ' + this.state.vatId);
         var validationUrl = "http://localhost:8080/api/validation/v1/vatid/" + this.state.vatId;
         console.log('validating input:' + validationUrl);
         
@@ -27,17 +31,32 @@ export default class FormComponent extends Component{
                 method: "GET",
             })
             .then((response) => response.json())
-            .then((result) => {
-                 //this.setState({ location: location });
-                console.log('callback: ' + result);    
+            .then((isVatIdValid) => {
+                console.log('callback::valid? ' + isVatIdValid);    
+                if (isVatIdValid) {
+                    this.setState({
+                        errors: { 
+                            vatIdValid: true,
+                            vatIdInValid: false
+                        }
+                    });
+                }
+                else {
+                    this.setState({
+                        errors: { 
+                            vatIdValid: false,
+                            vatIdInValid: true
+                        }
+                    });
+                }
             });
     }
 
     render() {
-        return <Form onSubmit="">
-            <Form.Group controlId="formBasicEmail">
+        return <Form>
+            <Form.Group>
                 <Form.Label>VAT ID</Form.Label>
-                <Form.Control id="vat-id-input" value={this.state.vatId} type="text" placeholder="Please enter vat id" />
+                <Form.Control autoFocus id="vat-id-input" isInvalid={this.state.errors.vatIdInValid} isValid={this.state.errors.vatIdValid} value={this.state.vatId} type="text" placeholder="Please enter vat id" onChange={this.handleChange} />
                 <Form.Text className="text-muted">
                 Currently supported countries are: DE, AT, GB, FR, DK, NL
                 </Form.Text>
